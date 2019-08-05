6b69fb8102797286f1f01b24583a5b168a4a2153
Mel Quesada <46976643+MelSkywalker@users.noreply.github.com>
2019-08-04 22:29:39 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,87 @@
+let map = null;
+
+function initMap(){
+    let location = new Object();
+    navigator.geolocation.getCurrentPosition(function(pos){
+        location.lat = pos.coords.latitude;
+        location.long = pos.coords.longitude;
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: location.lat, lng: location.long},
+            zoom: 15
+        });
+        getSchools(location);
+    });
+}
+
+function getSchools(location){
+    var pyrmont = new google.maps.LatLng(location.lat, location.long);
+    var request = {
+        location: pyrmont,
+        radius: '1500',
+        type: ['school'],
+        keyword: ['privada', 'secundaria']
+    };
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, callback);
+}
+
+function callback(results, status){
+    if(status == google.maps.places.PlacesServiceStatus.OK){
+        for(var i = 0 ; i < results.length ; i++ ){
+            console.log(results);
+            var place = results[i];
+            let price = createPrice(place.price_level);
+            let content = `<h3>${place.name}</h3>
+                        <h4>${place.vicinity}</h4>
+                        <p>Price: ${price}</br>
+                        Rating: ${place.rating}`;
+
+            var marker = new google.maps.Marker({
+                position: place.geometry.location,
+                map: map,
+                title: place.name
+            });
+
+            var infowindow = new google.maps.InfoWindow({
+                content: content
+            });
+
+            bindInfoWindow(marker, map, infowindow, content);
+            marker.setMap(map);
+        }
+    }
+}
+
+function bindInfoWindow(marker, map, infowindow, html){
+    marker.addListener('click', function() {
+        infowindow.setContent(html);
+        infowindow.open(map, this);
+    });
+}
+
+function createPrice(level){
+    if(level != "" && level != null){
+        let out = "";
+        for (var x = 0 ; x < level ; x++ ){
+            out += "$";
+        }
+        return out;
+    } else {
+        return "?";
+    }
+}
+
+
+
+// const url = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?'
+// const key = '&key=AIzaSyAU9glT1jY7iaGsjhviTrnl8RgJzfvZci4'
+// const fetch = require("node-fetch");
+
+// const searchSchools = () => {
+//     return fetch('https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=photos,formattedaddress,name,rating,opening_hours,geometry&key=AIzaSyAU9glT1jY7iaGsjhviTrnl8RgJzfvZci4')
+//     .then(res => res.json())
+//     .then(res => console.log(res))
+//     .catch(err => console.error(err));
+// };
+
+// searchSchools();
